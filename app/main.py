e8a10b0c7911c17c83f9789b48a6a917e646c7e5
MatheusNetto1 <netto.matheus123@outlook.com>
2025-10-08 03:35:21 -0300

    feat: carrega as mudanças na main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
 # app/main.py
 from fastapi import FastAPI
 from app.routes import auth_routes, product_routes
-from app.middlewares.error_middleware import add_error_handlers
 
 app = FastAPI(title="Python Backend")
 
 # Rotas
 app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
 app.include_router(product_routes.router, prefix="/products", tags=["Products"])
-
-# Middlewares de erro
-add_error_handlers(app)
