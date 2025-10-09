ea47a39c2f4de3c77bba193d4e20bb4ec39d64f8
MatheusNetto1 <netto.matheus123@outlook.com>
2025-10-09 02:08:44 -0300

    feat: adiciona mensagens personalizadas

---
+++ app/constants/messages.py
@@ -0,0 +1,13 @@
+# app/constants/messages.py
+MESSAGES = {
+    "NOT_FOUND": "Rota não encontrada",
+    "INTERNAL_ERROR": "Erro interno no servidor",
+    "UNAUTHORIZED": "Acesso negado. Requer autenticação.",
+    "INVALID_CREDENTIALS": "Credenciais inválidas",
+    "LOGIN_SUCCESS": "Login bem-sucedido",
+    "LOGOUT_SUCCESS": "Logout bem-sucedido",
+    "CART_EMPTY": "O carrinho está vazio.",
+    "INCOMPLETE_DATA": "Dados de usuário ou endereço incompletos.",
+    "ORDER_CONFIRMED": "Pedido confirmado com sucesso!",
+    "PRODUCT_NOT_FOUND": "Produto não encontrado",
+}
